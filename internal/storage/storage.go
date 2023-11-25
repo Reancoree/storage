e79e9cff0bd1265ffa06f3b1f1d9499bb96d33e7
Reancoree <reancoree@gmail.com>
2023-11-25 20:34:31 +0300

    it is done

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,20 +1,39 @@
 package storage
 
-import "github.com/Reancoree/storage/internal/file"
+import (
+	"fmt"
+
+	"github.com/Reancoree/storage/internal/file"
+	"github.com/google/uuid"
+)
 
 type Storage struct {
-
+	files map[uuid.UUID]*file.File
 }
 
 func NewStorage() *Storage {
-	return &Storage{}
+	return &Storage{
+		files: make(map[uuid.UUID]*file.File),
+	}
 } 
 
 func (s *Storage) Upload(filename string, blob []byte) (*file.File, error) {
-	return file.NewFile(filename, blob)
-	// if err != nil {
-	// 	return nil, err
-	// }
+	newFile, err := file.NewFile(filename, blob)
+	if err != nil {
+		return nil, err
+	}
+
+	s.files[newFile.ID] = newFile
 	
-	// return newFile, nil
+	return newFile, nil
 }
+
+func (s *Storage) GetById(fileId uuid.UUID) (*file.File, error) {
+	foundFile, ok := s.files[fileId]
+	if !ok {
+		return nil, fmt.Errorf("file %v not found", fileId)
+	}
+
+	return foundFile, nil
+}
+
